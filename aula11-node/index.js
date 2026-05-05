f3edd3b1e4666a29c9407ec7724eda0a6faf8584
Marcio Araya <marcio.araya@gmail.com>
2026-05-05 11:35:22 -0300

    exemplo programa javascript com node

---
+++ aula11-node/index.js
@@ -0,0 +1,8 @@
+// 1. Importa a biblioteca
+const prompt = require('prompt-sync')();
+
+// 2. Recebe o nome do usuário
+const nome = prompt('Digite seu nome: ');
+
+// 3. Exibe a mensagem de saudação
+console.log(`Olá, ${nome}`);
